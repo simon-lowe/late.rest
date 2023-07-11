cd5dcf0ea310da513bf0c7dc8b91359a77567476
simon-lowe <loewe.sim@gmail.com>
2023-07-11 12:17:04 +0200

    Finish up the first utility function

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -1,3 +1,15 @@
+#' Generate an group index list of length n with n_groups groups
+#'
+#' @param n Length of the list
+#' @param n_groups Number of groups
+#'
+#' @return An integer vector
+#' @export
+#'
+#' @examples
+#' # Group index list with 10 observations and 3 groups
+#' rand_n_list_group(10, 3)
+
 rand_n_list_group <- function(n, n_groups){
   rem <- n %% n_groups
   mult <- n %/% n_groups
